44ffe77fbe7aae71112d5c8b6dfb8bcd294b8d9b
Rafal Labunski <rafallabunski@gmail.com>
2025-10-16 10:45:25 +0200

    feat(ui): wrap routes with Container and mount Header/Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import Header from './components/views/Header';
+import Footer from './components/views/Footer';
+
+// strony
 import Home from './components/pages/Home';
 import Post from './components/pages/Post';
 import PostAdd from './components/pages/PostAdd';
@@ -9,14 +15,18 @@ import NotFound from './components/pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<Post />} />
-        <Route path="/post/add" element={<PostAdd />} />
-        <Route path="/post/edit/:id" element={<PostEdit />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/post/add" element={<PostAdd />} />
+          <Route path="/post/edit/:id" element={<PostEdit />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
     </BrowserRouter>
   );
 }
