a182f29debea09f3654dd1d876d68f0a1e42174c
Rafal Labunski <rafallabunski@gmail.com>
2025-10-16 10:59:08 +0200

    feat(home): header with Add post button and mount Posts grid

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,2 +1,20 @@
-const Home = () => <h1>Home</h1>;
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Posts from '../features/Posts';
+
+const Home = () => {
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-between mb-3">
+        <h1 className="mb-0">All posts</h1>
+        <Button as={Link} to="/post/add" variant="outline-primary">
+          Add post
+        </Button>
+      </div>
+
+      <Posts />
+    </>
+  );
+};
+
 export default Home;
