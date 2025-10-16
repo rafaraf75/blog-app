aabd08fc198a08c1d28759eb7986b84506a345d7
Rafal Labunski <rafallabunski@gmail.com>
2025-10-16 10:50:41 +0200

    ui: Bootstrap CSS + Container + Header/NavBar/Footer (routing osadzony)

---
+++ src/components/views/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="text-center text-muted py-4">
+    Copyright © BlogApp {new Date().getFullYear()}
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/views/NavBar.js
@@ -0,0 +1,22 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">Blog.app</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-nav" />
+        <Navbar.Collapse id="main-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+            {/* później dołożymy inne linki, np. Add Post */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
