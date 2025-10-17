2641f779368910486cc51840280d02bd7fe97acb
Rafal Labunski <rafallabunski@gmail.com>
2025-10-17 11:34:13 +0200

    feat(redux): add removePost action and reducer case

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -3,13 +3,17 @@ export const getAllPosts = state => state.posts;
 export const getPostById = (state, id) => state.posts.find(p => p.id === id);
 
 // actions
-const createActionName = actionName => `app/posts/${actionName}`;
+//const createActionName = actionName => `app/posts/${actionName}`;
+const REMOVE_POST = 'app/posts/REMOVE_POST';
 
 // action creators (dodamy później)
+export const removePost = id => ({ type: REMOVE_POST, payload: id });
 
 // reducer
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter(post => post.id !== action.payload);
     default:
       return statePart;
   }
